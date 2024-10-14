26c52155b927b9d9ead1d3796261f47dd65654f5
Mmmemla <zelma.lundgren@gritacademy.se>
2024-10-14 16:15:50 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+alert ( 123 )
+document.write ("Zelma")
+
+
+function doFunction() { console.log ("divide")}
+
+let foo = 0
+let bar = 0
+
+console.log (foo/bar)
+
+console.log ("You cant divide us!")
+
+
+function Print (message)  { console.log (message)}
+Print ("hello world")
